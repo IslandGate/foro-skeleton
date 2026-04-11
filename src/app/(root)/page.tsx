6ed648786360d37a9792f8f96835299b433de110
IslandGate <patrick.xu@online.island.edu.hk>
2026-04-11 17:52:50 +0800

    Four Ways Section added, need fix the arrangement

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,13 +1,14 @@
 import HeroAboutUs from "@/src/components/marketing-page/HeroAboutUs/HeroAboutUs";
+import HeroFourWays from "@/src/components/marketing-page/FourWaysSection/HeroFourWays";
 import HeroWelcome from "@/src/components/marketing-page/HeroWelcome";
 import HerousedByStudents from "@/src/components/marketing-page/HeroUsedByStudents/HeroUsedByStudents";
-import InfiniteCarousel from "@/src/components/marketing-page/HeroUsedByStudents/InfiniteCarousell";
 
 export default function page() {
   return (
     <>
       <HeroWelcome />
       <HeroAboutUs />
+      <HeroFourWays />
       <HerousedByStudents />
     </>
   );
