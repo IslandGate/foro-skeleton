2e7b302a988ef8534c2da96ab837dc88c66b3155
woof <hinyanryan@gmail.com>
2026-04-16 23:36:21 +0800

    Fixed import messages to be correct and removed vscode github extention text

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
-import HeroAboutUs from "@/components/marketing-page/HeroAboutUs/HeroAboutUs";
+import HeroAboutUs from "@/components/marketing-page/AboutUs/AboutUs";
 import HeroFourWays from "@/components/marketing-page/FourWaysSection/HeroFourWays";
-import HeroWelcome from "@/components/marketing-page/HeroWelcome";
-import HeroUsedByStudents from "@/components/marketing-page/HeroUsedByStudents/HeroUsedByStudents";
-=======
-import HeroAboutUs from "@/src/components/marketing-page/AboutUs/AboutUs";
-import HeroFourWays from "@/src/components/marketing-page/FourWaysSection/HeroFourWays";
-import HeroWelcome from "@/src/components/marketing-page/Welcome";
-import HeroUsedByStudents from "@/src/components/marketing-page/UsedByStudents/UsedByStudents";
->>>>>>> origin/main
+import HeroWelcome from "@/components/marketing-page/Welcome";
+import HeroUsedByStudents from "@/components/marketing-page/UsedByStudents/UsedByStudents";
   
 export default function Page() {
   return (
